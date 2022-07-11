cf088a307fc4946f5254a833f902ef26e898ffe2
Stuart Jennings <stuart.jennings@snyk.io>
2022-07-11 15:04:29 +0100

    add base files

---
+++ assets/script.js
@@ -0,0 +1,504 @@
+let colors = [
+    [
+        "145deb",
+        "brand.electricBlue"
+    ],
+    [
+        "ffffff",
+        "brand.white"
+    ],
+    [
+        "f6f7fb",
+        "brand.snow"
+    ],
+    [
+        "8383a8",
+        "brand.smoke"
+    ],
+    [
+        "555463",
+        "brand.steel"
+    ],
+    [
+        "21214c",
+        "brand.midnight"
+    ],
+    [
+        "e55578",
+        "brand.salmon"
+    ],
+    [
+        "e555ac",
+        "brand.hotPink"
+    ],
+    [
+        "ff78e1",
+        "brand.bubblegum"
+    ],
+    [
+        "f99048",
+        "brand.tiger"
+    ],
+    [
+        "f9c748",
+        "brand.summer"
+    ],
+    [
+        "ffe792",
+        "brand.daffodil"
+    ],
+    [
+        "168982",
+        "brand.darkTeal"
+    ],
+    [
+        "43b59a",
+        "brand.teal"
+    ],
+    [
+        "4bd6b5",
+        "brand.vibe"
+    ],
+    [
+        "007575",
+        "brand.deepSea"
+    ],
+    [
+        "14c4eb",
+        "brand.sky"
+    ],
+    [
+        "FFEEF2",
+        "brand.red.10"
+    ],
+    [
+        "FAC0CB",
+        "brand.red.20"
+    ],
+    [
+        "F48FA2",
+        "brand.red.30"
+    ],
+    [
+        "D8082D",
+        "brand.red.40"
+    ],
+    [
+        "B90020",
+        "brand.red.50"
+    ],
+    [
+        "8F0018",
+        "brand.red.60"
+    ],
+    [
+        "740000",
+        "brand.red.70"
+    ],
+    [
+        "FFF4ED",
+        "brand.orange.10"
+    ],
+    [
+        "FDD3B6",
+        "brand.orange.20"
+    ],
+    [
+        "FBB17F",
+        "brand.orange.30"
+    ],
+    [
+        "F99048",
+        "brand.orange.40"
+    ],
+    [
+        "E27122",
+        "brand.orange.50"
+    ],
+    [
+        "B6540B",
+        "brand.orange.60"
+    ],
+    [
+        "954002",
+        "brand.orange.70"
+    ],
+    [
+        "EAF1FF",
+        "brand.blue.10"
+    ],
+    [
+        "B5CCFA",
+        "brand.blue.20"
+    ],
+    [
+        "7FA7F5",
+        "brand.blue.30"
+    ],
+    [
+        "145DEB",
+        "brand.blue.40"
+    ],
+    [
+        "0F47C6",
+        "brand.blue.50"
+    ],
+    [
+        "07238B",
+        "brand.blue.60"
+    ],
+    [
+        "000052",
+        "brand.blue.70"
+    ],
+    [
+        "ECFCF8",
+        "brand.green.10"
+    ],
+    [
+        "B4E4D9",
+        "brand.green.20"
+    ],
+    [
+        "7BCDB9",
+        "brand.green.30"
+    ],
+    [
+        "43B59A",
+        "brand.green.40"
+    ],
+    [
+        "2D9283",
+        "brand.green.50"
+    ],
+    [
+        "16706B",
+        "brand.green.60"
+    ],
+    [
+        "004D54",
+        "brand.green.70"
+    ],
+    [
+        "ECDDFF",
+        "brand.purple.10"
+    ],
+    [
+        "CEB2F2",
+        "brand.purple.20"
+    ],
+    [
+        "B187E5",
+        "brand.purple.30"
+    ],
+    [
+        "9043C6",
+        "brand.purple.40"
+    ],
+    [
+        "722DC8",
+        "brand.purple.50"
+    ],
+    [
+        "471694",
+        "brand.purple.60"
+    ],
+    [
+        "200063",
+        "brand.purple.70"
+    ],
+    [
+        "f6f7fb",
+        "neutral.10"
+    ],
+    [
+        "e4e3e8",
+        "neutral.20"
+    ],
+    [
+        "d3d3d9",
+        "neutral.30"
+    ],
+    [
+        "c3c2cb",
+        "neutral.40"
+    ],
+    [
+        "b3b2bd",
+        "neutral.50"
+    ],
+    [
+        "a2a1af",
+        "neutral.60"
+    ],
+    [
+        "727184",
+        "neutral.70"
+    ],
+    [
+        "555463",
+        "neutral.80"
+    ],
+    [
+        "1c1c21",
+        "neutral.90"
+    ],
+    [
+        "ffffff",
+        "neutral.00"
+    ],
+    [
+        "f2ede6",
+        "severity.low.10"
+    ],
+    [
+        "d9d2ca",
+        "severity.low.20"
+    ],
+    [
+        "bfb7ac",
+        "severity.low.30"
+    ],
+    [
+        "a69e94",
+        "severity.low.40"
+    ],
+    [
+        "999084",
+        "severity.low.50"
+    ],
+    [
+        "807566",
+        "severity.low.60"
+    ],
+    [
+        "665e52",
+        "severity.low.70"
+    ],
+    [
+        "ffeacc",
+        "severity.medium.10"
+    ],
+    [
+        "f5d6ab",
+        "severity.medium.20"
+    ],
+    [
+        "ebba75",
+        "severity.medium.30"
+    ],
+    [
+        "d9a357",
+        "severity.medium.40"
+    ],
+    [
+        "cc913d",
+        "severity.medium.50"
+    ],
+    [
+        "a66100",
+        "severity.medium.60"
+    ],
+    [
+        "995900",
+        "severity.medium.70"
+    ],
+    [
+        "ffdbcc",
+        "severity.high.10"
+    ],
+    [
+        "fabda2",
+        "severity.high.20"
+    ],
+    [
+        "f5a887",
+        "severity.high.30"
+    ],
+    [
+        "f08b60",
+        "severity.high.40"
+    ],
+    [
+        "e5652e",
+        "severity.high.50"
+    ],
+    [
+        "bf3900",
+        "severity.high.60"
+    ],
+    [
+        "a63200",
+        "severity.high.70"
+    ],
+    [
+        "ffdbd9",
+        "severity.critical.10"
+    ],
+    [
+        "faa7a2",
+        "severity.critical.20"
+    ],
+    [
+        "f58c87",
+        "severity.critical.30"
+    ],
+    [
+        "f06760",
+        "severity.critical.40"
+    ],
+    [
+        "e54239",
+        "severity.critical.50"
+    ],
+    [
+        "bf0a00",
+        "severity.critical.60"
+    ],
+    [
+        "990800",
+        "severity.critical.70"
+    ],
+    [
+        "fff0c8",
+        "highlight.10"
+    ],
+    [
+        "ffe092",
+        "highlight.20"
+    ],
+    [
+        "ffd467",
+        "highlight.30"
+    ],
+    [
+        "d88100",
+        "highlight.50"
+    ]
+];
+var color, rgb, hsl;
+for(var i = 0; i < colors.length; i++)
+{
+    color = "#" + colors[i][0];
+    rgb = rgbValue(color);
+    hsl = hslValue(color);
+    colors[i].push(rgb[0], rgb[1], rgb[2], hsl[0], hsl[1], hsl[2]);
+}
+
+function getNeighbor(color) {
+    color = color.replace(/^#/, '');
+    color = color.toUpperCase();
+    if(color.length < 3 || color.length > 7)
+        return ["#000000", "Invalid Color: " + color, false];
+    if(color.length % 3 == 0)
+        color = "#" + color;
+    if(color.length == 4)
+        color = "#" + color.substr(1, 1) + color.substr(1, 1) + color.substr(2, 1) + color.substr(2, 1) + color.substr(3, 1) + color.substr(3, 1);
+
+    var rgb = rgbValue(color);
+    var r = rgb[0], g = rgb[1], b = rgb[2];
+    var hsl = hslValue(color);
+    var h = hsl[0], s = hsl[1], l = hsl[2];
+    var ndf1 = 0; ndf2 = 0; ndf = 0;
+    var cl = -1, df = -1;
+
+    for(var i = 0; i < colors.length; i++)
+    {
+        if(color == "#" + colors[i][0])
+            return ["#" + colors[i][0], colors[i][1], true];
+
+        ndf1 = Math.pow(r - colors[i][2], 2) + Math.pow(g - colors[i][3], 2) + Math.pow(b - colors[i][4], 2);
+        ndf2 = Math.pow(h - colors[i][5], 2) + Math.pow(s - colors[i][6], 2) + Math.pow(l - colors[i][7], 2);
+        ndf = ndf1 + ndf2 * 2;
+        if(df < 0 || df > ndf)
+        {
+            df = ndf;
+            cl = i;
+        }
+    }
+
+    return (cl < 0 ? ["#000000", "Invalid Color: " + color, false] : ["#" + colors[cl][0], colors[cl][1], false]);
+}
+
+function prepGetNeighbor(value) {
+    value = value.charAt(0) === '#' ? value.substring(1) : value;
+    if (value == '') {
+        die();
+    } else {
+        let hexPattern = /^(?:[0-9a-f]{3}){1,2}$/i;
+        let rgbPattern = /^rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)$/i;
+        document.getElementById('msg').innerHTML = '';
+        if (hexPattern.test(value)) {
+            //is hex
+            let color = getNeighbor(value)[0];
+            let name = getNeighbor(value)[1];
+            document.getElementById("container").style.backgroundColor = value;
+            document.getElementById("colorBox").style.backgroundColor = color;
+            document.body.style.backgroundColor = `#${value}`;
+            document.getElementById("colorName").innerHTML = name;
+            document.getElementById("colorValue").innerHTML = color;
+        } else if (rgbPattern.test(value)) {
+            //is rgb
+            let color = getNeighbor(rgbToHex(value))[0];
+            let name = getNeighbor(rgbToHex(value))[1];
+            document.getElementById("container").style.backgroundColor = value;
+            document.getElementById("colorBox").style.backgroundColor = color;
+            document.body.style.backgroundColor = value;
+            document.getElementById("colorName").innerHTML = name;
+            document.getElementById("colorValue").innerHTML = color;
+        } else {
+            die();
+        }
+    }
+}
+function componentFromStr(numStr, percent) {
+    var num = Math.max(0, parseInt(numStr, 10));
+    return percent ?
+        Math.floor(255 * Math.min(100, num) / 100) : Math.min(255, num);
+}
+function rgbToHex(rgb) {
+    var rgbRegex = /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+    var result, r, g, b, hex = "";
+    if ( (result = rgbRegex.exec(rgb)) ) {
+        r = componentFromStr(result[1], result[2]);
+        g = componentFromStr(result[3], result[4]);
+        b = componentFromStr(result[5], result[6]);
+
+        hex = "#" + (0x1000000 + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return hex;
+}
+function hslValue (color) {
+    var rgb = [parseInt('0x' + color.substring(1, 3)) / 255, parseInt('0x' + color.substring(3, 5)) / 255, parseInt('0x' + color.substring(5, 7)) / 255];
+    var min, max, delta, h, s, l;
+    var r = rgb[0], g = rgb[1], b = rgb[2];
+
+    min = Math.min(r, Math.min(g, b));
+    max = Math.max(r, Math.max(g, b));
+    delta = max - min;
+    l = (min + max) / 2;
+
+    s = 0;
+    if(l > 0 && l < 1)
+        s = delta / (l < 0.5 ? (2 * l) : (2 - 2 * l));
+
+    h = 0;
+    if(delta > 0)
+    {
+        if (max == r && max != g) h += (g - b) / delta;
+        if (max == g && max != b) h += (2 + (b - r) / delta);
+        if (max == b && max != r) h += (4 + (r - g) / delta);
+        h /= 6;
+    }
+    return [parseInt(h * 255), parseInt(s * 255), parseInt(l * 255)];
+}
+
+function rgbValue(color) {
+    return [parseInt('0x' + color.substring(1, 3)), parseInt('0x' + color.substring(3, 5)),  parseInt('0x' + color.substring(5, 7))];
+}
+function die(msg) {
+    msg = msg || 'Invalid input. HEX or RGB values only';
+    document.getElementById('msg').innerHTML = msg;
+}
+
+document.getElementById('colorVal').focus();
